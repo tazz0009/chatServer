5e3812e61cf7f4be896a308d972d9fb098781db6
tazz0009 <tazz0009@gmail.com>
2016-02-05 16:08:56 +0900

    controller 추가

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var io = require('socket.io')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -10,12 +9,15 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/signIn', function (req, res, next) {
-    console.log(req);
-    console.log('inputEmail : ' + req.body.inputEmail);
-    console.log('inputPassword : ' + req.body.inputPassword);
     res.render('chat', {
         title: 'Express',
         user: req.body.inputEmail
+    });
+});
+
+router.get('/controller', function (req, res, next) {
+    res.render('controller', {
+        title: 'controller'
     });
 });
 
